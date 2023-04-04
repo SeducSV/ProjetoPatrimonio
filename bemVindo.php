d9bd37d7be95eba0fbb554703a4a546852a1fa8c
Equipe TI - Seduc SV <121825599+SeducSV@users.noreply.github.com>
2023-04-04 11:57:18 -0300

    Add files via upload

---
+++ bemVindo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Departamento de Patrimônio</title>
+    <link rel="shortcut icon" href="pastas/imagens/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="pastas/css/bemVindo.css">
+    
+    <!-- index -->
+    <link rel="stylesheet" href="pastas/css/header.css">
+    <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+</head>
+
+<body  onresize="mudouTamanho()">
+    <?php include 'header.php';?>
+    <main>
+        <h1>Bem Vindo</h1>
+        <hr>
+    </main>
+</body>
+</html>
